dc5e392616f678c1603d564c5bc6812645e7f7b6
Riuqlav <valquirsolza.vs@gmail.com>
2023-07-25 23:16:15 +0200

    Fix text size

--- src/pages/About.js
+++ src/pages/About.js
@@ -25,10 +25,10 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 md:px-12 md:my-16 ">
+    <div className="container mx-auto px-4 md:px-12">
       <div className="flex flex-wrap justify-center" ref={containerRef}>
         <div className="m-10 max-w-full flex flex-col md:flex-row items-center transform transition duration-500 ease-in-out hover:scale-103">
-          <div className="w-full md:w-3/5 about-image my-10">
+          <div className="w-full md:w-3/5 about-image my-10 md:my-24">
             <img
               alt="About Placeholder"
                 className="w-full object-cover h-96"
@@ -38,7 +38,7 @@ const About = () => {
           <div className="w-full md:w-3/5 about-content">
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">About</div>
-              <p className="text-grey-darker text-lg">
+              <p className="text-grey-darker ">
                 We are a team of passionate developers who love building amazing
                 web applications. Our goal is to create software that is not
                 only functional but also beautiful and intuitive. We believe
