f5b87133101d099f166423288c8534b635a86a92
Riuqlav <valquirsolza.vs@gmail.com>
2023-07-25 14:40:02 +0200

    Create file Structure

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import TNRNFT from "./TNRnoSubp/X.jpg";
+import logo from "./TNR/The New Royals 2b1e1441ba8a448e94a1b4bd200165f8/X.jpg";
 
 const Header = () => (
   <header className="bg-black text-white">
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
       <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-        <img src={TNRNFT} alt="The New Royals" className="w-12 h-12 text-white p-2 rounded-full"/>
+        <img src={logo} alt="The New Royals" className="w-12 h-12 text-white p-2 rounded-full"/>
         <span className="ml-3 text-xl">The New Royals</span>
       </a>
       <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        {/* Navigation links can go here */}
+        <a href="#about" className="mr-5 hover:text-gray-900">About</a>
+        <a href="#events" className="mr-5 hover:text-gray-900">Events</a>
+        <a href="#links" className="mr-5 hover:text-gray-900">Links</a>
+        <a href="#contact" className="mr-5 hover:text-gray-900">Contact</a>
+
       </nav>
     </div>
   </header>
