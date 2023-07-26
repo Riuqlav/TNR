484f11475e7d33f32738f5a835a2d0f3775414e8
Riuqlav <valquirsolza.vs@gmail.com>
2023-07-26 12:19:16 +0200

    Update names

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,65 +9,59 @@ const Home = () => {
       id: 1,
       title: "About",
       content: "We are a team of passionate developers who love building amazing web applications. Our goal is to create software that is not only functional but also beautiful and intuitive. We believe that technology should be accessible to everyone, and we strive to make our products easy to use for people of all skill levels.",
-      img: "https://github.com/Riuqlav/calendar-dual/blob/master/src/assets/imgs/NFT/NFTART16.jpg",
+      img: require("../assets/imgs/NFt1.jpg"),
       link: "/about"
     },
     {
       id: 2,
       title: "Projects",
       content: "We have worked on a variety of projects, ranging from small websites to large-scale web applications. Our team has experience with a wide range of technologies, including React, Node.js, and MongoDB. We are always looking for new and exciting projects to work on, so if you have an idea for a web application, we would love to hear from you!",
-      img: "https://images.unsplash.com/photo-1549289524-06cf8837ace5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      img: require("../assets/imgs/NFt2.jpg"),
       link: "/projects"
     },
     {
       id: 3,
       title: "Ranks",
       content: "We are proud to have some of our projects ranked among the top web applications in the world. Our team has won several awards for our work, and we are constantly striving to improve our skills and create even better software. We believe that our success is due to our commitment to quality and our passion for what we do.",
-      img: "https://renaissancereframed.files.wordpress.com/2021/05/b6e5d-fireintheborgo.jpg",
+      img: require("../assets/imgs/NFt3.jpg"),
       link: "/ranks"
     },
     {
       id: 4,
       title: "David and God",
       content: "This striking piece portrays the seminal moment of divine connection between David and God. Its vivid colors and intricate detail illustrate the artist's skillful representation of this revered biblical event.",
-      img: "https://blog.artsper.com/wp-content/uploads/2022/04/New-Featured-Image-1200-x-675-6.jpg",
+      img: require("../assets/imgs/NFt4.jpg"),
       link: "/artwork4"
     },
     {
       id: 5,
       title: "Battle Scene",
       content: "A striking representation of a Renaissance battle, this painting captures the chaos and bravery of the time. The artist's skill is evident in the intricate detailing of the soldiers and the dynamic portrayal of action.",
-      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Battle_of_the_Milvian_Bridge_by_Giulio_Romano%2C_1520-24.jpg/2880px-Battle_of_the_Milvian_Bridge_by_Giulio_Romano%2C_1520-24.jpg",
+      img: require("../assets/imgs/NFt5.jpg"),
       link: "/artwork5"
     },
     {
       id: 6,
-      title: "Heavenly Ascension",
-      content: "This Renaissance painting beautifully depicts the heavenly ascension with serene and ethereal imagery. The intricate detailing of the celestial beings showcases the artist's skill and devotion.",
-      img: "https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80",
+      title: "The Last Supper",
+      content: "This iconic painting depicts the last meal shared by Jesus and his disciples before his crucifixion. The artist's use of light and shadow creates a dramatic effect, emphasizing the importance of this event in Christian history.",
+      img: require("../assets/imgs/NFt6.jpg"),
       link: "/artwork6"
     },
     {
       id: 7,
-      title: "Still Life with Skull",
-      content: "This still life painting features a hauntingly beautiful skull. The artist has brought a raw and morbid beauty to the forefront, emphasizing the transient nature of life.",
-      img: "https://images.unsplash.com/photo-1582561833407-b95380302a43?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=755&q=80",
+      title: "The Creation of Adam",
+      content: "This iconic painting depicts the last meal shared by Jesus and his disciples before his crucifixion. The artist's use of light and shadow creates a dramatic effect, emphasizing the importance of this event in Christian history.",
+      img: require("../assets/imgs/NFt7.jpg"),
       link: "/artwork7"
     },
     {
       id: 8,
-      title: "Church Interior",
-      content: "This baroque painting beautifully depicts the interior of a grand church. The artist's use of light and shadow brings depth to the image, and the intricate details highlight the architectural beauty of the era.",
-      img: "https://images.unsplash.com/photo-1551963423-0632e8a6977b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      title: "The Last Supper",
+      content: "This iconic painting depicts the last meal shared by Jesus and his disciples before his crucifixion. The artist's use of light and shadow creates a dramatic effect, emphasizing the importance of this event in Christian history.",
+      img: require("../assets/imgs/NFt8.jpg"),
       link: "/artwork8"
     },
-    {
-      id: 9,
-      title: "Heavenly Vision",
-      content: "Another beautiful representation of the heavens from the Renaissance era, this painting embodies the spiritual aspiration and religious devotion of the time, with celestial beings painted with fine detailing.",
-      img: "https://images.unsplash.com/photo-1602422701241-7ba4f6fc1712?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80",
-      link: "/artwork9"
-    },
+
   ], []);
 
 
@@ -78,18 +72,18 @@ const Home = () => {
       const direction = i % 2 === 0 ? "left" : "right";
 
       ScrollReveal().reveal(`.image-${post.id}`, {
-        delay: 300,
+        delay: 0,
         distance: "50px",
-        duration: 500,
+        duration: 0,
         easing: "ease-in-out",
         origin: direction,
         reset: true,
       });
 
       ScrollReveal().reveal(`.content-${post.id}`, {
-        delay: 500,
+        delay: 400,
         distance: "50px",
-        duration: 400,
+        duration: 300,
         easing: "ease-in-out",
         origin: direction === "left" ? "right" : "left",
         reset: true,
