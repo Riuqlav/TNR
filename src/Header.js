89c52a73c89f7e467faf63d53c6b1c786404b909
Riuqlav <valquirsolza.vs@gmail.com>
2023-07-27 13:51:07 +0200

    Add mobile menu

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from "./assets/imgs/X.jpg"
+import logo from "./assets/imgs/X.jpg";
 
-const Header = () => (
-  <header className='bg-gradient-to-r from-black to-yellow-500'>
-    <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
-      <Link to="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-        <img src={logo} alt="The New Royals" className="w-12 h-12 text-white p-2 rounded-full"/>
-        <span className="ml-3 text-xl">The New Royals</span>
-      </Link>
-      <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-        <Link to="/" className="mr-5 transition duration-1000 ease-in-out hover:text-white text-white">Home</Link>
-        <Link to="/About" className="mr-5 transition duration-1000 ease-in-out hover:text-white text-white">About</Link>
-        <Link to="/Projects" className="mr-5 transition duration-1000 ease-in-out hover:text-white text-white">Projects</Link>
-        <Link to="/Ranks" className="mr-5 transition duration-1000 ease-in-out hover:text-white text-white">Ranks</Link>
-        <Link to="/Vocabulary" className="mr-5 transition duration-1000 ease-in-out hover:text-white text-white">Vocabulary</Link>
-      </nav>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const hideMenu = () => setIsOpen(false);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    const visible = document.getElementById('header').offsetHeight;
+
+    if (currentScrollPos > visible) {
+      hideMenu();
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <header id="header" className='bg-gradient-to-r from-black to-yellow-500 relative'>
+      <div className="container mx-auto flex justify-between items-center p-1 md:p-1">
+        <Link to="/" className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+          <img src={logo} alt="The New Royals" className="w-12 h-12 text-white p-2 rounded-full" />
+          <span className="ml-1 text-xl">The New Royals</span>
+        </Link>
+        <button className='text-4xl md:hidden absolute top-0 right-0 m-1 mr-2 transition-colors duration-200 ' onClick={toggleMenu}>
+          {isOpen ? '' : '☰'}
+        </button>
+        <nav className={`fixed z-20 top-0 right-0 w-40 md:w-1/3 h-full bg-black bg-opacity-80 transform pt-0.4 ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-200 ease-in-out md:translate-x-0 md:relative md:bg-transparent md:flex md:items-center md:w-auto md:h-auto md:justify-end md:pt-0`}>
+          {isOpen && <button className='text-4xl md:hidden absolute top-0 right-0 m-2 transition-colors duration-200 ' onClick={toggleMenu}>✖</button>}
+          <Link to="/" className="block p-4 transition-colors duration-200 text-white" onClick={hideMenu}>Home</Link>
+          <Link to="/About" className="block p-4 transition-colors duration-200 text-white" onClick={hideMenu}>About</Link>
+          <Link to="/Projects" className="block p-4 transition-colors duration-200 text-white" onClick={hideMenu}>Projects</Link>
+          <Link to="/Ranks" className="block p-4 transition-colors duration-200 text-white" onClick={hideMenu}>Ranks</Link>
+          <Link to="/Vocabulary" className="block p-4 transition-colors duration-200 text-white" onClick={hideMenu}>Vocabulary</Link>
+        </nav>
+      </div>
+      {isOpen && <div className="fixed z-10 inset-0 bg-black opacity-50" onClick={hideMenu}></div>}
+    </header>
+  );
+};
 
 export default Header;
